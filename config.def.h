e6031a5c2227f96915d43e5d983cd524e9082798
Anthony Margerand <anthony.margerand@protonmail.com>
2023-03-06 21:25:57 +0100

    feat: add swallow patch

--- config.def.h
+++ config.def.h
@@ -2,11 +2,13 @@
 
 /* constants */
 #define TERMINAL "st"
+#define TERMCLASS "St"
 #define BROWSER "surf"
 
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating = 0;     /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "mono:pixelsize=12:antialias=true:autohint=true";
@@ -31,8 +33,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-    { NULL,       NULL,       NULL,       0,            False,       -1 },
+	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
+	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
+	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
+	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 };
 
 /* layout(s) */
